66df5fff1cf6d92b3f85cc119e507e3288c63f73
Matthew Chubb <matthew.chubb@zpg.co.uk>
2018-08-09 13:46:01 +0100

    Initial commit

---
+++ start-docker-container.sh
@@ -0,0 +1,3 @@
+docker run -it -v $PWD:/notebooks/mchubb  -p 8888:8888 tensorflow/tensorflow
+#docker run -it -p 8888:8888 tensorflow/tensorflow
+#nvidia-docker run -it -v $PWD:/notebooks/dojo -p 8888:8888 tensorflow/tensorflow:latest-gpu
