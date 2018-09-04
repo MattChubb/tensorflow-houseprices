ee9d343beb8f201aeec339039776c1269e33b3a5
Matthew Chubb <matthew.chubb@zpg.co.uk>
2018-09-04 17:35:09 +0100

    Tensorboard WIP docker script

--- start-docker-container.sh
+++ start-docker-container.sh
@@ -1,3 +1,4 @@
 docker run -it -v $PWD:/notebooks/mchubb  -p 8888:8888 tensorflow/tensorflow
+#docker run -it -v $PWD:/home/jovyan/mounted  -p 8888:8888 lspvic/tensorboard-notebook
 #docker run -it -p 8888:8888 tensorflow/tensorflow
 #nvidia-docker run -it -v $PWD:/notebooks/dojo -p 8888:8888 tensorflow/tensorflow:latest-gpu
