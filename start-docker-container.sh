c4838d50ec3a585753d7acab0c09178e4c64c4a7
Matthew Chubb <matthew.chubb@zpg.co.uk>
2018-11-06 17:33:55 +0000

    06Nov2018

--- start-docker-container.sh
+++ start-docker-container.sh
@@ -1,4 +1,5 @@
-docker run -it -v $PWD:/notebooks/mchubb  -p 8888:8888 tensorflow/tensorflow
+#docker run -it -v $PWD:/notebooks/mchubb  -p 8888:8888 tensorflow/tensorflow
+docker run -it --cpus=2 -v $PWD:/notebooks/mchubb  -p 8888:8888 tensorflow/tensorflow
 #docker run -it -v $PWD:/home/jovyan/mounted  -p 8888:8888 lspvic/tensorboard-notebook
 #docker run -it -p 8888:8888 tensorflow/tensorflow
 #nvidia-docker run -it -v $PWD:/notebooks/dojo -p 8888:8888 tensorflow/tensorflow:latest-gpu
